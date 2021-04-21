dc149f2c4d21d998b623a21ac7b1ece865afbbce
John Paxton <pax@speedingplanet.com>
2021-04-21 11:24:55 -0400

    Added effect hook demo

--- src/demos/DemosView.tsx
+++ src/demos/DemosView.tsx
@@ -3,6 +3,7 @@ import { NavLink, Route } from 'react-router-dom';
 import ComponentCommunication from './ComponentCommunication';
 import ComponentProps from './ComponentProps';
 import ComponentState from './ComponentState';
+import EffectHook from './EffectHook';
 import EventHandling from './EventHandling';
 
 export default function DemosView(): JSX.Element {
@@ -48,6 +49,14 @@ export default function DemosView(): JSX.Element {
                 Component Communication
               </NavLink>
             </li>
+            <li>
+              <NavLink
+                to="/demos/effect-hook"
+                activeClassName="font-weight-bold"
+              >
+                Effect Hook
+              </NavLink>
+            </li>
           </ul>
         </div>
         <div className="col">
@@ -62,6 +71,9 @@ export default function DemosView(): JSX.Element {
           </Route>
           <Route path="/demos/component-communication">
             <ComponentCommunication />
+          </Route>
+          <Route path="/demos/effect-hook">
+            <EffectHook />
           </Route>
         </div>
       </div>
