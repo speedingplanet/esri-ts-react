4d87fd3b66100b30cabd2116db309a7743c14994
John Paxton <pax@speedingplanet.com>
2021-04-22 09:15:50 -0400

    Added owners

--- src/demos/RenderList.tsx
+++ src/demos/RenderList.tsx
@@ -30,15 +30,15 @@ oranges/3
 */
 
 let originalShips = [
-  { name: 'Enterprise-D', id: 1 },
-  { name: 'Sulaco', id: 2 },
-  { name: 'Nostromo', id: 3 },
-  { name: 'Alabama', id: 4 },
-  { name: 'Minnow', id: 5 },
-  { name: 'Serenity', id: 6 },
-  { name: 'Pequod', id: 7 },
-  { name: 'Revenge', id: 8 },
-  { name: 'Black Pearl', id: 9 },
+  { owner: 'United Federation of Planets', name: 'Enterprise-D', id: 1 },
+  { owner: 'Weyland-Yutani', name: 'Sulaco', id: 2 },
+  { owner: 'Weyland-Yutani', name: 'Nostromo', id: 3 },
+  { owner: 'US Government', name: 'Alabama', id: 4 },
+  { owner: 'Jonas Grumby', name: 'Minnow', id: 5 },
+  { owner: 'Mal Reynolds', name: 'Serenity', id: 6 },
+  { owner: 'Ahab', name: 'Pequod', id: 7 },
+  { owner: 'Dread Pirate Roberts', name: 'Revenge', id: 8 },
+  { owner: 'Various', name: 'Black Pearl', id: 9 },
 ];
 
 export default function RenderList(): ReactElement {
