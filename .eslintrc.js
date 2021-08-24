360ea9c822bf648aa8a50e36484a14275ac18c0d
John Paxton <pax@speedingplanet.com>
2021-08-24 10:05:04 -0400

    Configuration changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,31 +14,54 @@ module.exports = {
     ecmaVersion: 2021,
     sourceType: 'module',
     project: './tsconfig.json',
+    // requireConfigFile: false,
   },
 
   extends: [
-    // 'standard',
-    // 'standard-with-typescript',
-    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'standard-with-typescript',
+    // 'plugin:@typescript-eslint/recommended',
     'react-app',
     'react-app/jest',
     'plugin:react-hooks/recommended',
   ],
   rules: {
     'array-bracket-spacing': [ 'error', 'always' ],
+    // '@typescript-eslint/array-bracket-spacing': [ 'error', 'always' ],
     'comma-dangle': [ 'error', 'always-multiline' ],
+    '@typescript-eslint/comma-dangle': [ 'error', 'always-multiline' ],
     indent: [ 'error', 2, { MemberExpression: 1 } ],
+    '@typescript-eslint/indent': [ 'error', 2, { MemberExpression: 1 } ],
     'max-len': [ 'error', { code: 90, comments: 120 } ],
+    // '@typescript-eslint/max-len': [ 'error', { code: 90, comments: 120 } ],
     'newline-per-chained-call': [ 'error', { ignoreChainWithDepth: 2 } ],
+    /*
+    '@typescript-eslint/newline-per-chained-call': [
+      'error',
+      { ignoreChainWithDepth: 2 },
+    ],
+    */
     'object-curly-spacing': [ 'error', 'always' ],
+    '@typescript-eslint/object-curly-spacing': [ 'error', 'always' ],
     'object-property-newline': [
       'error',
       { allowAllPropertiesOnSameLine: true },
     ],
+    /*
+    '@typescript-eslint/object-property-newline': [
+      'error',
+      { allowAllPropertiesOnSameLine: true },
+    ],
+    */
     'prefer-const': 'off',
+    '@typescript-eslint/prefer-const': 'off',
     semi: [ 'error', 'always' ],
+    '@typescript-eslint/semi': [ 'error', 'always' ],
     'space-before-function-paren': [ 'error', 'never' ],
+    '@typescript-eslint/space-before-function-paren': [ 'error', 'never' ],
     'space-in-parens': [ 'error', 'always' ],
+    // '@typescript-eslint/space-in-parens': [ 'error', 'always' ],
+    // '@babel/semi': 'warn',
   },
   ignorePatterns: [ 'node_modules' ],
 };
