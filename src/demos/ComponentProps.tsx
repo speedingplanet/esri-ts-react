aab01ec90e967a51e65b8a42de7143dc2010e3e4
John Paxton <pax@speedingplanet.com>
2021-08-25 14:05:58 -0400

    Demo update

--- src/demos/ComponentProps.tsx
+++ src/demos/ComponentProps.tsx
@@ -8,7 +8,7 @@ export default function ComponentProps(): JSX.Element {
       <div className="row">
         <div className="col">
           <BootstrapCard title="Component Props" bodyClasses="text-center">
-            <DisplayValue foo={counter} />
+            <DisplayValueDestructured foo={counter} />
             <button
               className="btn btn-primary"
               onClick={() => setCounter( counter + 1 )}
@@ -26,7 +26,12 @@ interface DisplayValueProps {
   foo: number;
 }
 
-// function DisplayValue( props: DisplayValueProps ): JSX.Element {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface FlexibleInterface {
+  [key: string]: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function DisplayValue( props: DisplayValueProps ): JSX.Element {
   return <p>{props.foo}</p>;
 }
@@ -34,4 +39,9 @@ function DisplayValue( props: DisplayValueProps ): JSX.Element {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function DisplayValueDestructured( { foo }: DisplayValueProps ): JSX.Element {
   return <p>{foo}</p>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function DisplayValueInline( props: { foo: number } ): JSX.Element {
+  return <p>{props.foo}</p>;
 }
